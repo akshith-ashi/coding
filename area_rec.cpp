363a55c478425a0eaf60291bfa2ee85bbb922a42
akshith-ashi <akshithashi07@gmail.com>
2025-08-25 20:04:55 +0530

    Added program to find the area of Rectangle

---
+++ area_rec.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float length, width;
+    cout << "Enter length and width: ";
+    cin >> length >> width;
+    cout << "Area of rectangle = " << length * width << endl;
+    return 0;
+}
